62f6d38e3c368cf111b729e654f4a77f7a1ce0b2
s25509 <s25509@pjwstk.edu.pl>
2024-06-08 19:06:47 +0200

    a bit of the first controller

---
+++ Kolos_Code_First/Controllers/ClientController.cs
@@ -0,0 +1,66 @@
+﻿using Kolos_Code_First.Context;
+using Kolos_Code_First.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kolos_Code_First.Controllers;
+
+[Route("/api/[controller]")]
+[ApiController]
+public class ClientController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    public ClientController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet("{idClient:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetClientWithAllDataAsync(int idClient)
+    {
+        var Client = await _context.Clients
+            .Where(c => c.IdClient == idClient)
+            .Include(c => c.IdSaleNavigation)
+            .ThenInclude(sa => sa.IdSubscription)
+            .Include(c => c.Prescriptions)
+            .ThenInclude(cr => pr.PrescriptionMedicaments)
+            .ThenInclude(cm => pm.IdMedicamentNavigation)
+            .Select(c => new ClientDto
+            {
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Email = c.Email,
+                Prescriptions = c.Prescriptions
+                    .OrderByDescending(cr => pr.DueDate)
+                    .Select(cr => new SubscriptionDto
+                    {
+                        IdPrescription = pr.IdPrescription,
+                        Date = pr.Date,
+                        DueDate = pr.DueDate,
+                        Doctor = new DoctorDto
+                        {
+                            IdDoctor = pr.IdDoctorNavigation.IdDoctor,
+                            FirstName = pr.IdDoctorNavigation.FirstName,
+                            LastName = pr.IdDoctorNavigation.LastName,
+                            Email = pr.IdDoctorNavigation.Email
+                        },
+                        Medicaments = pr.PrescriptionMedicaments.Select(cm => new MedicamentDto
+                            {
+                                IdMedicament = pm.IdMedicamentNavigation.IdMedicament,
+                                Name = pm.IdMedicamentNavigation.Name,
+                                Description = pm.IdMedicamentNavigation.Description,
+                                Type = pm.IdMedicamentNavigation.Type
+                            }
+                        ).ToList()
+                    }
+                ).ToList()
+            })
+            .FirstOrDefaultAsync();
+
+        if (Client is null) return NotFound($"Client with ID: {idClient} not found");
+
+        return Ok(Client);
+    }
+}
---
+++ Kolos_Code_First/DTOs/SubscriptionsDTOs.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlTypes;
+
+namespace Kolos_Code_First.Models
+{
+    public class ClientDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<SubscriptionDto> Subscriptions { get; set; }
+    }
+
+    public class SubscriptionDto
+    {
+        public int IdSubscription { get; set; }
+        public string Name { get; set; }
+        public SqlMoney TotalPaidAmount { get; set; }
+    }
+}
